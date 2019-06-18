0f97f73b537a3c59b06ed45452efebcfd1830ac4
Alen <1697260287@qq.com>
2019-06-18 16:08:50 +0800

    init commint

--- src/App.js
+++ src/App.js
@@ -1,27 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
+import './index.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.hanleItemDelect = this.hanleItemDelect.bind(this);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment >
+        <div>
+          <input 
+            className="input"
+            value={this.state.inputValue} 
+            onChange={this.handleInputChange}
+          />
+          <button
+            onClick={this.handleButtonClick}
+          >Submit</button>
+        </div>
+        <ul>
+          {this.randerTodoList()}
+        </ul>
+      </Fragment>
     );
+  }
+  randerTodoList() {
+    return this.state.list.map((item, index) => {
+      return (
+        <Fragment
+          key={index}
+        >
+          <TodoItem 
+            content={item} 
+            index={index}
+            delectItem={this.hanleItemDelect}
+          />
+          {/*<li 
+            key={index} 
+            onClick={this.hanleItemDelect.bind(this, index)}
+            dangerouslySetInnerHTML={{__html: item}}
+          >
+            {item}
+          </li>*/}
+        </Fragment>
+      )
+    })
+  }
+
+  handleInputChange(e) {
+    // this.setState({
+    //   inputValue: e.target.value
+    // })
+    const value = e.target.value;
+    this.setState(() => {
+      return {
+        inputValue: value
+      }
+    })
+  };
+  handleButtonClick() {
+    // this.setState({
+    //   list: [...this.state.list, this.state.inputValue],
+    //   inputValue: ''
+    // })
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
+      inputValue: ""
+    }))
+  };
+  hanleItemDelect(index){
+    let list = [...this.state.list];
+    list.splice(index, 1);
+
+    this.setState({
+      list: list
+    })
   }
 }
 
